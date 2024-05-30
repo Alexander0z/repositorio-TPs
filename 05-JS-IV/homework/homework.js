e99bed82a372d79a5c90f8e99da48082c3e58338
Alexander0z <alexandergameroz@gmail.com>
2024-05-30 06:37:01 -0300

    Homework JSIV realizado

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -31,6 +31,10 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+
+  // var Nada;
+  // Nada = objeto[metodo]()
+  // return Nada;
   objeto[metodo]();
 }
 
